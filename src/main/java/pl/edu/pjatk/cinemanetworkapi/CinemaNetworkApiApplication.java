f9ded20941dad64c2ec5771bd351ac53d1e4e109
Emil Stryszawski <stryszawskiemil@gmail.com>
2022-05-29 09:57:06 +0200

    Initial commit

---
+++ src/main/java/pl/edu/pjatk/cinemanetworkapi/CinemaNetworkApiApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pjatk.cinemanetworkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaNetworkApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaNetworkApiApplication.class, args);
+    }
+
+}
